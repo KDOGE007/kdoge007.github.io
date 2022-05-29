b1146d05a67b5e324b09129f9f932093666ea968
Kharn Tupmongkol <kharnthorn@gmail.com>
2022-05-29 19:28:03 +0100

    first commit

---
+++ secret/staff.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Staff roster and contact</title>
+</head>
+<body>
+  <h1>Contacts</h1>
+  <div>
+    <h2>Name: Staff 1</h2>
+    <h3>Contact: 021 xxx xxx</h3>
+  </div>
+  <div>
+    <h2>Name: Staff 2</h2>
+    <h3>Contact: 027 xxx xxxx</h3>
+  </div>
+  <hr>
+  <h2>Rosters</h2>
+  <div class="center margin-top margin-btm">
+      <!-- google calender -->
+    <iframe src="https://calendar.google.com/calendar/embed?src=pa4sp2dmcq3i4okqf1m4usfbe0%40group.calendar.google.com&ctz=Pacific%2FAuckland" 
+      style="border: 0" 
+      width="600" 
+      height="450" 
+      frameborder="0" 
+      scrolling="no"></iframe>
+    </div>
+</body>
+</html>
